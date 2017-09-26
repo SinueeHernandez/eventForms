49fd981b99e6baaba502d9c348b06fb21e05a6b4
Sinuee Hernández <sinuee@gmail.com>
2017-09-25 21:55:06 -0500

    Now working retriving list
    with out authentication

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import { AngularFireDatabase, FirebaseObjectObservable, FirebaseListObservable } from 'angularfire2/database';
+import { Subject } from 'rxjs/Subject';
 
 @Component({
   selector: 'app-root',
@@ -8,9 +9,34 @@ import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/databa
 })
 export class AppComponent {
   title = 'Sinuee Hello Angular';
-  items: FirebaseListObservable<any[]>;
-  
+  item: FirebaseObjectObservable<any>;
+  items: FirebaseListObservable<any>;
+  messages: FirebaseListObservable<any>;
+  sizeSubject: Subject<any>;
+
   constructor(db: AngularFireDatabase) {
-    this.items = db.list('/items');
+    this.messages = db.list('/messages');
+    this.sizeSubject = new Subject();
+    this.items = db.list('/items', {
+      query: {
+        orderByChild: 'size',
+        equalTo: this.sizeSubject
       }
+    });
+  }
+  addItem(newName: string) {
+    this.messages.push({ text: newName });
+  }
+  updateItem(key: string, newText: string) {
+    this.messages.update(key, { text: newText });
+  }
+  deleteItem(key: string) {
+    this.messages.remove(key);
+  }
+  deleteEverything() {
+    this.messages.remove();
+  }
+  filterBy(size: string) {
+    this.sizeSubject.next(size);
+  }
 }
