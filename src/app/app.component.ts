dc6bfde7d3f7e09b4ad0d867ffefef51f1f58bce
Sinuee Hernández <sinuee@gmail.com>
2017-09-26 13:32:45 -0500

    User Auth with email and password
    User Auth with email and password implemented and working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { AngularFireDatabase, FirebaseObjectObservable, FirebaseListObservable } from 'angularfire2/database';
 import { Subject } from 'rxjs/Subject';
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-root',
@@ -13,11 +16,22 @@ export class AppComponent {
   items: FirebaseListObservable<any>;
   messages: FirebaseListObservable<any>;
   sizeSubject: Subject<any>;
+  user: Observable<firebase.User>;
+  email: string;
+  password: string;
+  loged: boolean;
 
-  constructor(db: AngularFireDatabase) {
-    this.messages = db.list('/messages');
+  constructor(private db: AngularFireDatabase, private afAuth: AngularFireAuth) {
+    this.email = '';
+    this.password = '';
+    this.user = afAuth.authState;
+    if (this.user != null)
+      this.loadData();
+  }
+  loadData() {
+    this.messages = this.db.list('/messages');
     this.sizeSubject = new Subject();
-    this.items = db.list('/items', {
+    this.items = this.db.list('/items', {
       query: {
         orderByChild: 'size',
         equalTo: this.sizeSubject
@@ -38,5 +52,42 @@ export class AppComponent {
   }
   filterBy(size: string) {
     this.sizeSubject.next(size);
+  }
+  login() {
+    this.afAuth.auth.signInWithEmailAndPassword(this.email, this.password)
+      //.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+      .then(result => {
+        this.email = '';
+        this.password = '';
+        this.loged = true;
+        this.loadData();
+      })
+      .catch(err => {
+        let errorMessage = err.message;
+        alert(errorMessage);
+        console.log(err);
+      });
+  }
+
+  logout() {
+    this.afAuth.auth.signOut().then(result =>{
+      this.loged = false;
+      this.loadData();
+    });
+  }
+
+  signUp() {
+    this.afAuth.auth.createUserWithEmailAndPassword(this.email, this.password)
+      .catch(function (error) {
+        // Handle Errors here.
+        var errorCode = error['code'];
+        var errorMessage = error.message;
+        if (errorCode == 'auth/weak-password') {
+          alert('The password is too weak.');
+        } else {
+          alert(errorMessage);
+        }
+        console.log(error);
+      });
   }
 }
